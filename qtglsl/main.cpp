92bc01928bb739f6fe272e83940357142413450e
degarashi <slice013@gmail.com>
2013-05-03 19:47:04 +0900

    GLXのテスト関数をOpenGLの初期化後に呼ぶよう変更

--- qtglsl/main.cpp
+++ qtglsl/main.cpp
@@ -1,33 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QMessageBox>
-#include "glresource.hpp"
-#include "glx.hpp"
-
-void TestGLX() {
-	GLEffect gle;
-	gle.readGLX("test.glx");
-// 	gle.setTechnique("TestTech");
-// 	gle.setPass("TestPass");
-// 	gle.applySetting();
-// 	gle.setPass("AnotherPass");
-// 	gle.applySetting();
-// 	gle.setVDecl({0, 0, 4, VSem.POSITION});
-// 	gle.setUniform(vec4(1,2,3,4), "the_param");
-// 	gle.setMacro("the_entry", 128);
-
-// 	glDrawElements(...);
-// 	gle.saveParams();
-// 	gle.restoreParams();
-}
 
 int main(int argc, char *argv[]) {
-	// OpenGL関数群を読み込む
-	LoadXGLFunc();
-	TestGLX();
-
 	QApplication a(argc, argv);
 	MainWindow w;
+	TestGL* testGL = new TestGL();
+	w.setCentralWidget(testGL);
 	w.show();
 
 	try {
--- qtglsl/testgl.cpp
+++ qtglsl/testgl.cpp
@@ -1,9 +1,32 @@
 #include "mainwindow.h"
+#include "glresource.hpp"
+#include "glx.hpp"
+
+void TestGLX() {
+	GLEffect gle;
+	gle.readGLX("test.glx");
+// 	gle.setTechnique("TestTech");
+// 	gle.setPass("TestPass");
+// 	gle.applySetting();
+// 	gle.setPass("AnotherPass");
+// 	gle.applySetting();
+// 	gle.setVDecl({0, 0, 4, VSem.POSITION});
+// 	gle.setUniform(vec4(1,2,3,4), "the_param");
+// 	gle.setMacro("the_entry", 128);
+
+// 	glDrawElements(...);
+// 	gle.saveParams();
+// 	gle.restoreParams();
+}
 
 TestGL::TestGL(QWidget* parent) {
 
 }
 
-void TestGL::initializeGL() {}
+void TestGL::initializeGL() {
+	// OpenGL関数群を読み込む
+	LoadXGLFunc();
+	TestGLX();
+}
 void TestGL::resizeGL(int, int) {}
 void TestGL::paintGL() {}
